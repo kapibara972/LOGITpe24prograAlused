8798bbc4e75b8c55c1092d852b53e0fafd299c27
Opilane TTHK <opilane@tthk.ee>
2025-03-03 13:52:51 +0200

    kuk

---
+++ kuk/kuk/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace kuk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //peab saama sisestada nime 
+            // if ja else kontrollib, kas on nimi või on tühi 
+            // kui leiate kuskilt teksti värviliseksteha, siis kasutage 
+            Console.WriteLine("sisesta nimi");
+            string name = Console.ReadLine();
+
+            if (name != "")
+            {
+                Console.WriteLine("tere" + name);
+            }
+
+            else
+                {
+                Console.WriteLine("EI SISESTAND NIMI");
+                 }
+        }
+    }
+}
