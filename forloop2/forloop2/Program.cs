6f1d27f3267c3095723b07f3619d46ceeb8fec0d
Opilane TTHK <opilane@tthk.ee>
2025-03-03 15:50:59 +0200

    forloop2
    forloop2

---
+++ forloop2/forloop2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace forloop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            /* Ülesanne 3
+               eelnevaid teadmisis kasutades kirjuta programm, mis väljastakse:
+            kill-koll kill- koll killadi-koll kill-koll kill- koll
+            killadi- koll kill-koll kill-koll killadi-koll*/
+            // for loop for loopi sees
+
+            int arv = Int16.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+                Console.WriteLine("See rida on nr {0}", i);
+                string a = "kill-koll";
+                string b = "killadi-koll";
+            
+
+            // iga kord kui hakkab pihta suurem for loop, siis seal sees
+            // olevas esimeses for loopis on kill-koll kaks korda
+            // teises for loopis killadi-koll ühe korra 
+
+
+
+                for(int j = 1; j <= 2; j++)
+                {
+                    Console.WriteLine(a);
+                }
+
+                for (int k = 0; k <= 1; k++)
+                {
+                    Console.WriteLine)´(b);
+                }
+            }
+        }
+    }
+}
