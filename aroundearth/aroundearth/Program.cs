95615a19d0b5a28c87f6ce69d2f1074b508c8ab0
Opilane TTHK <opilane@tthk.ee>
2025-03-03 15:02:56 +0200

    aroundearth
    aroundearth

---
+++ aroundearth/aroundearth/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace aroundearth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // vaja arvutada, mitu kahe eurost münti läheb ümber maa
+            // ,aa ümbermõõt, tuleb välja arvutada raadius abil
+
+            double coinDiameter = 25.75; //mm
+            double eartRadius = 6378000000; //mm
+
+            double maaÜmber = 2 * Math.PI * eartRadius;
+
+            double vastus = maaÜmber / coinDiameter;
+            Console.WriteLine("vaja läheb " + vastus);
+        }
+    }
+}
