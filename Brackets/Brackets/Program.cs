a0d645bc5b69f6c2be3ba97e1c5eace34e9d5033
Opilane TTHK <opilane@tthk.ee>
2025-03-03 13:58:15 +0200

    Bracest
    Bracest

--- Brackets/Brackets/Program.cs
+++ Brackets/Brackets/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("KUI PALJU ON 3+8 / (4-2) * 4 = `?" +)
+
+                //MIKS ARVUTI SELLISELT ARVUTAB 
+                double calculation1 = 3 + 8 / (4 - 2) * 4;
+             double calculation2 = (3 + 8 ) / (4 - 2) * 4;
+            double calculation3 = (3 + 8) / (4 - 2) * 4;
+            double calculation4 = (3.0 + 8.0) / (4.0 - 2.0) * 4.0);
+
+            Console.WriteLine("Vastus: " + calculation1);
+            Console.WriteLine("Vastus: " + calculation2);
+            Console.WriteLine("Vastus: " + calculation3);
+            Console.WriteLine("Vastus: " + calculation4);
         }
     }
 }
