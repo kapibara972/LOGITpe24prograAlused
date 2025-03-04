ddb5e674ef4e18dccc6a0efd9ef5dd7fe12b9d2f
kapibara972 <nicolasgoferman@gmail.com>
2025-03-04 11:24:54 +0200

    Add files via upload

---
+++ MethodCall.cs
@@ -0,0 +1,23 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // kutsud esile metodi jalgratas
+
+            Jalgratas();
+
+        }
+
+            static void Jalgratas()
+            {
+                Console.WriteLine("nüüd oled jalgrata meetodis");
+            }
+
+
+        
+    }
+}
