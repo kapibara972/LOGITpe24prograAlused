5346efbb905df4245b0cb90e5aa201bff877b4cf
kapibara972 <nicolasgoferman@gmail.com>
2025-03-04 18:44:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace koduTöö
+{
+       internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sisesta esimene arv:");
+            double arv1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Sisesta teine arv:");
+            double arv2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Vali tehe (+, -, *, /):");
+            char mark = Convert.ToChar(Console.ReadLine());
+
+            double tulemus;
+
+            if (mark == '+')
+            {
+                tulemus = arv1 + arv2;
+                Console.WriteLine("Tulemus: " + tulemus);
+            }
+            else if (mark == '-')
+            {
+                tulemus = arv1 - arv2;
+                Console.WriteLine("Tulemus: " + tulemus);
+            }
+            else if (mark == '*')
+            {
+                tulemus = arv1 * arv2;
+                Console.WriteLine("Tulemus: " + tulemus);
+            }
+            else if (mark == '/')
+            {
+                if (arv2 != 0)
+                {
+                    tulemus = arv1 / arv2;
+                    Console.WriteLine("Tulemus: " + tulemus);
+                }
+                else
+                {
+                    Console.WriteLine("Bum bum oled nulliga ei saa jagada! ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vale tehe!");
+            }
+
+
+
+        }
+        
+
+    }
+
+}
