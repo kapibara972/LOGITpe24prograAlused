166bfa60d88dffbaa1bfd0859b0af1c8224515cd
kapibara972 <nicolasgoferman@gmail.com>
2025-03-04 11:45:10 +0200

    Add files via upload

---
+++ methodCall2.cs
@@ -0,0 +1,47 @@
+﻿namespace methodCall2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // siin kasutad if ja else 
+            // mõlemaks valikuks peab välja kutsuma meetodi
+            // teemaks paaris ja paartu arvud
+            // paaris ja paritu arvu tekst in teise meetodi sees
+
+
+//EvenNumber meetod
+
+            Console.WriteLine("Hello, World!");
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (int.IsEvenInteger(number))
+            {
+                EvenNumber(number);
+            }
+            else
+            {
+                oddNumber(number);
+            }
+
+            static void EvenNumber(int nr)
+            {
+                Console.WriteLine("tegemist on paaris arvuga" + nr);
+            }
+
+
+            
+//oddNumber meetod
+
+            static void oddNumber(int nr)
+            {
+                Console.WriteLine("tegemist on paaris arvuga" + nr);
+            }
+            
+
+        }
+    }
+}
