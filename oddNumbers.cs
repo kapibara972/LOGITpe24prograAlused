78ef848b6e06a9ecd86e6fa5925df304dd346761
kapibara972 <nicolasgoferman@gmail.com>
2025-03-04 11:11:58 +0200

    Add files via upload

---
+++ oddNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace oddNumbrs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //komsool küsib numbrit ja siin muudetakse 
+            // see int andmetüübiks
+
+            // teha if ja else- ga kontroll
+            // kus saame teada, et kas nr
+            // on paaris või paaritu 
+            // kasuta operaatorit
+
+
+            Console.WriteLine("ütle number ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+                Console.WriteLine("paaris");
+
+            else
+                Console.WriteLine("paaritu");
+                
+
+
+        }
+    }
+}
