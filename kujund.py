98330d1461ef435ec485017c8030ec9f1dd2cb96
kapibara972 <nicolasgoferman@gmail.com>
2025-04-14 13:01:57 +0300

    Add files via upload

--- kujund.py
+++ kujund.py
@@ -1,67 +1,89 @@
-#kiirjuta programm mis
-# küsib kasutajalt tema lemmikvärv
-# küsib kasutajalt tema lemmikkujund (ruut, ring, ristkülik, võrdhaarne kolmnutk, täisnurk kolmnurk)
-# joonistab kasutajale vastava kujundi tema värvi
-from turtle import*
-from random import randint
+
+# NB! kasutajalt sisendi võtmine peab igal juhul tsüklis olema niikaua kuni programm saab õige sisendi
+# NB! kasutajasisend tuleb töödelda standardkujuke, nime puhul esisuurtäht, programmi jaoks vajalik sisend ühtlasele kujule jne
+# kirjuta programm mis
+#
+# küsib kasutajalt kui vana ta on
+# küsib kasutajalt kui pikk ta on meetrites
+# küsib kasutajalt tema ees ja perekonnanime
+# küsib kasutajalt kas talle meeldivad geomeetrilised kujundid
+# kui kasutaja vastab jah, siis küsitakse kasutajalt terve rida küsimusi järgnevate kujundite kohta: (ring, ruut, kolmnurk, ovaal)
+# - kas talle meeldib <kujund>
+# - kui kasutaja vastab <kujund>i kohta jah, siis peab programm meelde jätma et kasutaja tahab seda kujundit eraldi muutujas
+# - kasutajalt küsitakse ka tema lemmikvärvi kohta, mille jaoks antakse kasutajale inglisekeelne valik
+# - seejärel joonistab programm kõik kujundid mis said "jah" vastuse kasutaja lemmikvärviga ekraanile +
+# - kujundite küljepikkus tuleb läbi korrutada kasutaja pikkusega. kujundite standardküljepikkus on 100px
+# - Viimasena joonistatakse ekraanile teksti abil sõnum "palun"<kasutajanimi>"siin on sinu lemmikkujundid"<värv> värviga
+
+
+from tkinter import*
+raam= Tk()
+raam.title(" i are windo")
+tahvel=Canvas(raam, width=1000,height=1000)
+
+vana=""
+pikk=""
+enimi=""
+pnimi=""
+geoküs=""
+värv=""
+
+#kujundid
+ring=""
+ruut=""
+kolmnurk=""
+ovaal=""
+
+while(vana==""):
+    vana= int(input("kui vana oled"))
+    
+while(pikk==""):
+    pikk=float(input("kui pikk oled meetrites"))
+    
+while(enimi==""):
+    enimi=input("ütle eeesnimi")
+
+while(pnimi==""):
+    pnimi=input("ütle perekpnna nimi")
+    
+
+while(geoküs==""):
+    geoküs=input("kas meeldivad geo kujundid")
+    if geoküs=="ei":
+        print (" käi perkele")
+        
+while (ring==""):
+    ring=input("meeldib ring").lower()
+while (ruut==""):
+    ruut=input("meeldib ruut").lower()
+while (kolmnurk==""):
+    kolmnurk=input("meeldib kolmnurk").lower()
+while (ovaal==""):
+    ovaal=input("meeldib ovaal").lower()
+    
+    
+while(värv==""):
+    värv=input("ütle värv in inka keeles")
 
 
 
-print(" ütle lemmik värv, ütle inglise keeles")
-varv = input()
-print ("ütle lemmikmkujud (ruut, ring, ristkülik, võrdhaarne kolmnurk, täisnurkne kolmnurk)")
-kujund = input()
 
-if (kujund == "ruut"):
-    color (varv)
-    fd (100)
-    lt(90)
-    fd(100)
-    lt(90)
-    fd(100)
-    lt(90)
-    fd(100)
-    
-elif ( kujund =="ring"):
-    color ( varv)
-    begin_fill()
-    circle(100)
-    end_fill()
-
-elif ( kujund =="võrdhaarne kolmnurk"):
-    color (varv)
-    fd(100)
-    lt(120)
-    fd(100)
-    lt(120)
-    fd(100)
-    
-    
-elif ( kujund =="täisnurkne kolmnurk"):
-    color (varv)
-    fd(100)
-    rt(120)
-    fd(200)
-    rt(150)
-    fd(180)
-elif (kujund =="ristkülik"):
-    color(varv)
-    fd(200)
-    lt(90)
-    fd(100)
-    lt(90)
-    fd(200)
-    lt(90)
-    fd(100)
-    
-    
-    
-exitonclick()
+if(ring=="jah"):
+    tahvel.create_oval(pikk*100, pikk*100, pikk*300, pikk*300,outline=värv, fill=värv)
+    tahvel.create_text(490,375,text=("palun",enimi,"siin on sinu lemmikkujundid",värv, "värviga"), fill=värv)
+if(ruut=="jah"):
+    tahvel.create_polygon(pikk*700,pikk*700,pikk*900,pikk*900,outline=värv, fill=värv)
+    tahvel.create_text(490,375,text=("palun",enimi,"siin on sinu lemmikkujundid",värv, "värviga"), fill=värv)
+if(kolmnurk=="jah"):
+    tahvel.create_polygon(pikk*200,pikk*600,pikk*200,pikk*700,pikk*300,pikk*700,outline=värv, fill=värv)
+    tahvel.create_text(490,375,text=("palun",enimi,"siin on sinu lemmikkujundid",värv, "värviga"), fill=värv)
+if(ovaal=="jah"):
+     tahvel.create_oval(pikk*600,pikk*600,pikk*700,pikk*900,outline=värv, fill=värv)
+     tahvel.create_text(490,375,text=("palun",enimi,"siin on sinu lemmikkujundid",värv, "värviga"), fill=värv)
+          
 
 
 
+tahvel.pack()
+raam.mainloop()
 
-    
-    
-    
-    
